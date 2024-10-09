96115c0ec7530cb1ea57185e1ed863f967efd2dd
Javier Ontiveros <hola@javo.work>
2024-10-09 15:59:54 -0600

    feat: added help page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,8 +3,8 @@ import darkLogo from "assets/images/wgu-logo-dark.svg";
 
 const Header = ({ title }) => {
   return (
-    <header className="relative h-28">
-      <div className="bg-primary-250 container fixed top-0 z-20 mx-auto flex items-center gap-6 px-6 pb-8 pt-14">
+    <header className="relative h-32">
+      <div className="container fixed top-0 z-20 mx-auto flex items-center gap-4 bg-primary-250 px-6 pb-8 pt-8">
         <img src={darkLogo} width="85" height="20" alt="" />
         <span className="text-xl font-bold">|</span>
         {title && <h1 className="text-lg">{title}</h1>}
