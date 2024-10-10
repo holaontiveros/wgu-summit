4d37f74632ab2833bc9e8e5b22630dcf819a61b3
Javier Ontiveros <hola@javo.work>
2024-10-10 13:26:34 -0600

    chore: added skip waiting button

--- src/routes/Egg.jsx
+++ src/routes/Egg.jsx
@@ -16,10 +16,26 @@ const Egg = () => {
       }
     });
   };
+
+  const handleSkipWaiting = () => {
+    console.log("Skip waiting");
+
+    // this will trigger the "skip waiting" for the service worker that's is pending to be registered so it's registered right away
+    navigator.serviceWorker.ready.then((registration) => {
+      registration.waiting.postMessage({ type: "SKIP_WAITING" });
+    });
+  };
+
   return (
     <div className="flex min-h-screen w-full flex-col items-center justify-center bg-gray-500">
       <h1 className="mb-28 text-8xl">🐔🥚</h1>
       Current permission result: {permisionResult && <p>{permisionResult}</p>}
+      <button
+        className="btn btn-secondary"
+        onClick={askForNotificationPermission}
+      >
+        Ask for notificationsn permission
+      </button>
       <button
         className="btn btn-secondary"
         onClick={askForNotificationPermission}
